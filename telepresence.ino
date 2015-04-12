537ec4b73272a260c1807ec40681dfa243af5280
hubmartin <hub.martin@gmail.com>
2015-04-12 19:22:04 +0200

    housekeeping

--- telepresence.ino
+++ telepresence.ino
@@ -1,55 +1,64 @@
 #include<stdlib.h>
-
 #include <crc16.h>
 #include <espduino.h>
 #include <FP.h>
 #include <mqtt.h>
 #include <rest.h>
 #include <ringbuf.h>
-
 #include <SoftwareSerial.h>
-
 #include <Timer.h>
-
-//#include <Servo.h>
 #include <ServoTimer1.h>
-
 #include <avr/wdt.h>
 
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
+// EXAMPLE CONFIGURATION
+// ---------------------------
 
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 5
+// Enable DS18B20 temperature
+//#define TEMPERATURE
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+// Enable sleep after temperature send
+//#define POWER_DOWN
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+// ---------------------------
+
+
+#ifdef TEMPERATURE
+  #include <OneWire.h>
+  #include <DallasTemperature.h>
+  
+  // Data wire is plugged into pin 2 on the Arduino
+  #define ONE_WIRE_BUS 5
+  // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+  OneWire oneWire(ONE_WIRE_BUS);
+
+  // Pass our oneWire reference to Dallas Temperature. 
+  DallasTemperature sensors(&oneWire);
+#endif
+
 uint32_t timestamp = 0;
 
-#define POWER_DOWN
-
-//Servo servo;
 ServoTimer1 servo;
-//Servo servo2;
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+// Pin 13 has an LED
 int led = 13;
 
+// Servo actual position
 int pos = 0; 
 
+// Software debug serial port
 SoftwareSerial debugPort(2, 3); // RX, TX
+
+// Link ESP to HW USART0
 ESP esp(&Serial, /*&debugPort,*/ 4);
 
+// MQTT instance
 MQTT mqtt(&esp);
 
+// Some helper flags
 boolean wifiConnected = false;
 boolean mqttConnectedFlag = false;
 
+// Wifi callback
 void wifiCb(void* response)
 {
   uint32_t status;
@@ -74,17 +83,16 @@ void wifiCb(void* response)
 void mqttConnected(void* response)
 {
   debugPort.println("Connected");
-  //mqtt.subscribe("/hubacek/servo"); //or mqtt.subscribe("topic"); /*with qos = 0*/
-  //mqtt.subscribe("/hubacek/led");
+  mqtt.subscribe("/hubacek/servo"); //or mqtt.subscribe("topic"); /*with qos = 0*/
+  mqtt.subscribe("/hubacek/led");
   //mqtt.publish("/hubacek/status", "start");
-  
   mqttConnectedFlag = true;
-
 }
+
+
 void mqttDisconnected(void* response)
 {
   debugPort.print("Disconnected!");
-  
   mqttConnectedFlag = false;
   
   /*
@@ -95,8 +103,8 @@ void mqttDisconnected(void* response)
   setup();
   #endif
   */
-
 }
+
 void mqttData(void* response)
 {
   RESPONSE res(response);
@@ -136,14 +144,12 @@ void mqttPublished(void* response)
   }
   
  
-  
+  // Experimental power down after temperature send
   #ifdef POWER_DOWN
-  
   
   debugPort.println("MQTT disconnecting, powering down");
   mqtt.disconnect();
-  
-  
+    
   mqttConnectedFlag = false;
   
   digitalWrite(13, HIGH);
@@ -154,7 +160,6 @@ void mqttPublished(void* response)
   wdtlib_pwr_down(WDTO_2S);
   
   setup();
-
   #endif
   
 }
@@ -196,19 +201,18 @@ esp.enable();
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT); 
-
-servo.attach(9);  
-//servo2.attach(8);
-
-Serial.begin(19200); 
-debugPort.begin(19200);
-
-while(setupESP() == 1)
-{
-    debugPort.println("Trying reinit ESP again in 1sec...");
-    esp.disable();
-    delay(1000);
-}
+  
+  servo.attach(9);  
+  
+  Serial.begin(19200); 
+  debugPort.begin(19200);
+  
+  while(setupESP() == 1)
+  {
+      debugPort.println("Trying reinit ESP again in 1sec...");
+      esp.disable();
+      delay(1000);
+  }
 
    
    //set timer2 interrupt at 8kHz
@@ -224,30 +228,15 @@ while(setupESP() == 1)
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE2A);
   
-  
-  sensors.begin();
+  #ifdef TEMPERATURE
+    sensors.begin();
+  #endif
   
   timestamp = millis();
  
 }
 
 
-
-// The interrupt will blink the LED, and keep
-// track of how many times it has blinked.
-int ledState = LOW;
-volatile unsigned long blinkCount = 0; // use volatile for shared variables
-
-void blinkLED(void)
-{
-  if (ledState == LOW) {
-    ledState = HIGH;
-    blinkCount = blinkCount + 1;  // increase when LED turns on
-  } else {
-    ledState = LOW;
-  }
-  digitalWrite(led, ledState);
-}
 
 ISR(TIMER2_COMPA_vect){//timer1 interrupt 8kHz toggles pin 9
 //generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
@@ -259,49 +248,48 @@ ISR(TIMER2_COMPA_vect){//timer1 interrupt 8kHz toggles pin 9
   // 800 100ms
   if(i == 160)
   {
-    i = 0;
+      i = 0;
+      
+    if(pos > servoActPos)
+    {
+     servoActPos++;
+     servo.write(servoActPos);
+    } 
     
-  if(pos > servoActPos)
-  {
-   servoActPos++;
-   servo.write(servoActPos);
-  } 
-  
-  if(pos < servoActPos)
-  {
-    servoActPos--;
-    servo.write(servoActPos);
-  } 
-    
-   
-   
+    if(pos < servoActPos)
+    {
+      servoActPos--;
+      servo.write(servoActPos);
+    } 
   }
 }
 
 
- char charVal[10]; 
+ 
 
 // the loop routine runs over and over again forever:
 void loop() {
- 
+  // Small buffer for float to char conversion
+  #define CHAR_BUFFER_SIZE 20
+  static char charVal[CHAR_BUFFER_SIZE]; 
   
   esp.process();
   
   if(millis() - timestamp > 5000 && mqttConnectedFlag)
   {
-    //mqtt.disconnect();
-    
-    sensors.requestTemperatures();
-    float t = sensors.getTempCByIndex(0);
-    
-    dtostrf(t, 4, 1, charVal); 
-    debugPort.print("Temp: ");
-    debugPort.println(charVal);
+    #ifdef TEMPERATURE
+      sensors.requestTemperatures();
+      float t = sensors.getTempCByIndex(0);
+      
+      dtostrf(t, 4, 1, charVal); 
+      debugPort.print("Temp: ");
+      debugPort.println(charVal);
+    #else
+      strncpy(charVal, CHAR_BUFFER_SIZE, "Hello MQTT!");
+    #endif
     
     mqtt.publish("/hubacek/temp", charVal);
     timestamp = millis();
   }
-
-   
-  
+ 
 }
